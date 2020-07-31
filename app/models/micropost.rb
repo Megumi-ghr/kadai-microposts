f4395d5c8ab76c734ddd59dacff5366e6434449a
megumi.yasugahira <yasugahira.megumi@imagicalab.co.jp>
2020-07-31 05:06:37 +0000

    favor, unfavor

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+
+  has_many :reverses_of_favorites, class_name: "Favorite"
+  has_many :liked, through: :reverses_of_favorites, source: :user, dependent: :destroy
+
 end
